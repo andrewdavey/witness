d0d94fca5e19a65a21288292cecdac81d43e132b
Ryan Roberts <ryansroberts@gmail.com>
2011-06-14 17:19:53 +0100

    naming problems

---
+++ src/Witness.Tests/ExecutableHeadersTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Witness.Tests
+{
+    public class ExecutableHeadersTest
+    {
+        protected ExecutableHeaders headers;
+        protected bool executed;
+
+        protected void HeaderSet(string key)
+        {
+            headers = new ExecutableHeaders();
+            headers.SetHeaders(new Dictionary<string, string>{{key,"function() {doThis();}"}});
+            headers.Add("doThis", () => executed = true);
+        }
+    }
+}
---
+++ src/Witness/DotNetContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+using Jurassic;
+
+namespace Witness
+{
+    public class DotNetContext : DynamicObject
+    {
+        private ScriptEngine engine = new ScriptEngine();
+
+        public DotNetContext Add(string key, object argument)
+        {
+            engine.SetGlobalValue(key,argument);
+            return this;
+        }
+
+        public DotNetContext Add(string key, Action argument)
+        {
+            engine.SetGlobalFunction(key,argument);
+            return this;
+        }
+
+        public void Run(string script)
+        {
+            engine.Execute(script);
+        }
+    }
+}
