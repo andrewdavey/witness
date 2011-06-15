9c2a78401624ee6916b31eddbfe52c054422781d
Andrew Davey <andrew@equin.co.uk>
2011-06-15 12:52:21 +0100

    Only bind the automation iframe load event to the current context. The iframe now exists for each Scenario (instead of new for each run). So can be reused and controlling the UI will be easier.

--- src/Witness/demo/specs/index.js
+++ src/Witness/demo/specs/index.js
@@ -1,7 +1,29 @@
-﻿describe("demo page", {
-    "given": loadPage("/demo/index.htm"),
-    when: click("#btn"),
-    "then": {
+﻿describe("demo page",
+{
+    given: loadPage("/demo/index.htm"),
+    when: click("#hello"),
+    then: {
         "#message": should.haveText("Hello, world!")
+    }
+},
+{
+    given: loadPage("/demo/index.htm"),
+    when: click("button.number:eq(0)"),
+    then: {
+        "#message": should.haveText("Number is 1")
+    }
+},
+{
+    given: loadPage("/demo/index.htm"),
+    when: click("button.number:eq(1)"),
+    then: {
+        "#message": should.haveText("Number is 2")
+    }
+},
+{
+    given: loadPage("/demo/index.htm"),
+    when: click("button.number:eq(2)"),
+    then: {
+        "#message": should.haveText("Number is 3")
     }
 });
