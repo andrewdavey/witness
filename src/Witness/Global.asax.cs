d632142b05aad1aac9005d6fe8ac63c5f3a94c4e
Andrew Davey <andrew@equin.co.uk>
2011-07-12 17:44:05 +0100

    Add Global.asax routing set up.

---
+++ src/Witness/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Witness.RequestHandlers;
+
+namespace Witness
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            MapRoutes(RouteTable.Routes);
+        }
+
+        void MapRoutes(RouteCollection routes)
+        {
+            // Any URL starting "_witness" is mapped to a Witness controller.
+            routes.MapRoute(
+                "Witness",
+                "_witness/{controller}/{action}"
+            );
+
+            // All other URLs are proxied to their original target.
+            // (If the Witness cookie has been set up.)
+            routes.Add(
+                "Proxy",
+                new Route(
+                    "{*any}", // Match any URL
+                    null, // No defaults required
+                    new { constraint = new RequireWitnessCookieConstraint() },
+                    new WitnessRouteHandler<ProxyHandler>()
+                )
+            );
+
+            // When new browser session requests root URL they have no Witness cookie.
+            // So display the runner page.
+            routes.MapRoute(
+                "Init",
+                "", // The root URL.
+                new { controller = "Runner", action = "Index" }
+            );
+        }
+    }
+}
---
+++ src/Witness/RequireWitnessCookieConstraint.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Routing;
+
+namespace Witness
+{
+    public class RequireWitnessCookieConstraint : IRouteConstraint
+    {
+        public bool Match(System.Web.HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return routeDirection != RouteDirection.IncomingRequest
+                || httpContext.Request.Cookies["_witness"] != null;
+        }
+    }
+}
