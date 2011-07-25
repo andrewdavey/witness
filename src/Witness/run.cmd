0116cb2deb05e0e2c189b7cf9934c73e34735b5e
Andrew Davey <andrew@equin.co.uk>
2011-07-25 17:48:24 +0100

    Add Witness run batch file for the redist build.

---
+++ src/Witness/run.cmd
@@ -0,0 +1,10 @@
+echo off
+set port=1234
+SET ExecPath=%ProgramFiles(x86)%
+IF "%ExecPath%"=="" SET ExecPath=%ProgramFiles%
+
+echo Starting Witness web server at http://localhost:%port%
+
+"%ExecPath%\iis express\iisexpress.exe" "/path:%cd%" /port:%port%
+
+echo on
