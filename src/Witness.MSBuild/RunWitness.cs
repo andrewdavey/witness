f7fbbacba5376e53294161a3605aa6c21d0e2a6f
Andrew Davey <andrew@equin.co.uk>
2011-07-27 17:13:37 +0100

    Find Witness runner and PhantomJS by looking in same directory as the Witness.MSBuild.dll.

--- src/Witness.MSBuild/RunWitness.cs
+++ src/Witness.MSBuild/RunWitness.cs
@@ -1,18 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
+using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
-using System.Linq.Expressions;
 
 namespace Witness.MSBuild
 {
+    /// <summary>
+    /// Starts Witness and the website under test in IIS Express. Runs PhantomJS to execute the specification runner.
+    /// All output from these processes is piped to the task's log output.
+    /// </summary>
     public class RunWitness : Task
     {
+        public RunWitness()
+        {
+            witnessRootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        /// <summary>
+        /// The directory containing the installed Witness resources.
+        /// </summary>
+        string witnessRootDirectory;
+
         /// <summary>
         /// Path to the specifications directory.
         /// </summary>
@@ -33,7 +44,6 @@ namespace Witness.MSBuild
         /// <summary>
         /// File system path to the Witness application root.
         /// </summary>
-        [Required]
         public string Witness { get; set; }
 
         /// <summary>
@@ -69,9 +79,16 @@ namespace Witness.MSBuild
             if (WitnessPort == 0) WitnessPort = TcpHelpers.GetFreeTcpPort(9000);
             if (WebsitePort == 0) WebsitePort = TcpHelpers.GetFreeTcpPort(WitnessPort + 1);
 
+            if (string.IsNullOrEmpty(Witness))
+            {
+                Witness = Path.Combine(witnessRootDirectory, "web");
+            }
+
             EnsureAbsolutePath(() => Witness);
             EnsureAbsolutePath(() => Website);
             EnsureAbsolutePath(() => Specifications);
+
+            EnsurePhantomJS();
         }
 
         void EnsureAbsolutePath(Expression<Func<string>> property)
@@ -88,35 +105,27 @@ namespace Witness.MSBuild
             action();
         }
 
+        void EnsurePhantomJS()
+        {
+            if (string.IsNullOrEmpty(PhantomJS))
+            {
+                PhantomJS = Path.Combine(witnessRootDirectory, "phantomjs\\phantomjs.exe");
+            }
+
+            if (Path.IsPathRooted(PhantomJS) == false)
+            {
+                PhantomJS = Path.GetFullPath(PhantomJS);
+            }
+
+            if (File.Exists(PhantomJS) == false)
+            {
+                throw new FileNotFoundException("PhantomJS executable not found at " + PhantomJS);
+            }
+        }
+
         Process StartIISExpress(string websitePath, int port)
         {
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = GetIISExpressExePath(),
-                Arguments = CreateIISArguments(websitePath, port),
-                RedirectStandardError = true,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                UseShellExecute = false
-            };
-            var process = Process.Start(startInfo);
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            process.OutputDataReceived += (sender, e) =>
-            {
-                if (e.Data != null)
-                {
-                    Log.LogMessage(e.Data);
-                }
-            };
-            process.ErrorDataReceived += (sender, e) =>
-            {
-                if (e.Data != null)
-                {
-                    Log.LogError(e.Data);
-                }
-            };
-            return process;
+            return StartProcessThatPipesToLog(GetIISExpressExePath(), CreateIISArguments(websitePath, port));
         }
 
         string GetIISExpressExePath()
@@ -141,33 +150,7 @@ namespace Witness.MSBuild
 
         Process StartPhantomJS()
         {
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = PhantomJS,
-                Arguments = GetPhantomJSArguments(Specifications, WitnessPort, WebsitePort),
-                RedirectStandardError = true,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                UseShellExecute = false
-            };
-            var process = Process.Start(startInfo);
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            process.OutputDataReceived += (sender, e) =>
-            {
-                if (e.Data != null)
-                {
-                    Log.LogMessage(e.Data);
-                }
-            };
-            process.ErrorDataReceived += (sender, e) =>
-            {
-                if (e.Data != null)
-                {
-                    Log.LogError(e.Data);
-                }
-            };
-            return process;
+            return StartProcessThatPipesToLog(PhantomJS, GetPhantomJSArguments(Specifications, WitnessPort, WebsitePort));
         }
 
         string GetPhantomJSArguments(string specsPath, int witnessPort, int websitePort)
@@ -178,6 +161,38 @@ namespace Witness.MSBuild
                 websitePort,
                 specsPath
             );
+        }
+
+        Process StartProcessThatPipesToLog(string exeFilename, string arguments)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = exeFilename,
+                Arguments = arguments,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            var process = Process.Start(startInfo);
+            PipeProcessToLog(process);
+            return process;
+        }
+
+        void PipeProcessToLog(Process process)
+        {
+            process.BeginOutputReadLine();
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (e.Data == null) return; // MSBuild throws an exception if the message is null. So we'll just skip those.
+                Log.LogMessage(e.Data);
+            };
+
+            process.BeginErrorReadLine();
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (e.Data == null) return; // MSBuild throws an exception if the message is null. So we'll just skip those.
+                Log.LogError(e.Data);
+            };
         }
     }
 }
