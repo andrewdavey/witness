5d3136b9d150b06c8216cc7d7dfbd69bc2fe47ea
Andrew Davey <andrew@equin.co.uk>
2011-08-04 16:01:01 +0100

    Move class into it's own file.

--- src/Witness.Specs.Remote/Configuration.cs
+++ src/Witness.Specs.Remote/Configuration.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.Composition;
 using Jurassic;
-using Jurassic.Library;
-using System.ComponentModel.Composition;
 
 namespace Witness.Specs.Remote
 {
@@ -14,33 +9,6 @@ namespace Witness.Specs.Remote
         public void Configure(ScriptEngine scriptEngine)
         {
             scriptEngine.SetGlobalValue("utils", new Utils(scriptEngine));
-        }
-
-        class Utils : ObjectInstance
-        {
-            public Utils(ScriptEngine engine)
-                : base(engine)
-            {
-                this.PopulateFunctions();
-            }
-
-            [JSFunction]
-            public static int serverfunctionthatreturns42()
-            {
-                return 42;
-            }
-
-            [JSFunction]
-            public static int serverfunctionthatreturns43()
-            {
-                return 43;
-            }
-
-            [JSFunction]
-            public static void servermethodthatthrowsanexception()
-            {
-                throw new Exception("bang");
-            }
         }
     }
 }
---
+++ src/Witness.Specs.Remote/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Jurassic.Library;
+
+namespace Witness.Specs.Remote
+{
+    /// <summary>
+    /// For use by the Witness remote specs.
+    /// </summary>
+    class Utils : ObjectInstance
+    {
+        public Utils(ScriptEngine engine)
+            : base(engine)
+        {
+            this.PopulateFunctions();
+        }
+
+        [JSFunction]
+        public static int serverfunctionthatreturns42()
+        {
+            return 42;
+        }
+
+        [JSFunction]
+        public static int serverfunctionthatreturns43()
+        {
+            return 43;
+        }
+
+        [JSFunction]
+        public static void servermethodthatthrowsanexception()
+        {
+            throw new Exception("bang");
+        }
+    }
+}
